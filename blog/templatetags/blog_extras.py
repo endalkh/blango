4c86815bc36e150415c803e28c9cacc6f715e45b
endalkh belete <endalkhbel@gmail.com>
2022-04-24 10:21:48 +0000

    add changes

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -29,3 +29,11 @@ def author_details(author, current_user=None):
         suffix = ""
 
     return format_html('{}{}{}', prefix, name, suffix)
+
+@register.simple_tag
+def row():
+  return format_html('<div class="row">')
+  
+@register.simple_tag
+def endrow():
+  return format_html("</div>")
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import redirect
+from blog.forms import CommentForm
 from blog.models import Post
 def index(request):
-  posts = Post.objects.filter(published_at__lte=timezone.now())
-  return render(request, "blog/index.html", {"posts": posts})
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    if request.user.is_active:
+        if request.method == "POST":
+            comment_form = CommentForm(request.POST)
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.content_object = Post
+                comment.creator = request.user
+                comment.save()
+                return redirect(request.path_info)
+        else:
+            comment_form = CommentForm()
+    else:
+        comment_form = None
+    return render(
+        request, "blog/post-detail.html", {"post": posts, "comment_form": comment_form}
+    )
