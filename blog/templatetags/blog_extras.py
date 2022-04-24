23971a12259553e96fd673bc9753a910ecbcfc85
endalkh belete <endalkhbel@gmail.com>
2022-04-24 12:49:13 +0000

    fix: issue

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,11 +1,20 @@
+from django import template
 from django.contrib.auth import get_user_model
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
+
 user_model = get_user_model()
+register = template.Library()
 
-
-def author_details(author):
+@register.filter
+def author_details(author, current_user=None):
     if not isinstance(author, user_model):
         # return empty string as safe default
         return ""
+
+    if author == current_user:
+        return format_html("<strong>me</strong>")
 
     if author.first_name and author.last_name:
         name = f"{author.first_name} {author.last_name}"
@@ -22,9 +31,17 @@ def author_details(author):
     return format_html('{}{}{}', prefix, name, suffix)
 
 @register.simple_tag
-def row():
-  return format_html('<div class="row">')
-  
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
 @register.simple_tag
 def endrow():
   return format_html("</div>")
+
+@register.simple_tag
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+@register.simple_tag
+def endcol():
+    return format_html("</div>")
--- blog/views.py
+++ blog/views.py
@@ -3,21 +3,14 @@ from django.utils import timezone
 from django.shortcuts import redirect
 from blog.forms import CommentForm
 from blog.models import Post
+from django.shortcuts import render, get_object_or_404
+
+from django.shortcuts import render
+
 def index(request):
-    posts = Post.objects.filter(published_at__lte=timezone.now())
-    if request.user.is_active:
-        if request.method == "POST":
-            comment_form = CommentForm(request.POST)
-            if comment_form.is_valid():
-                comment = comment_form.save(commit=False)
-                comment.content_object = Post
-                comment.creator = request.user
-                comment.save()
-                return redirect(request.path_info)
-        else:
-            comment_form = CommentForm()
-    else:
-        comment_form = None
-    return render(
-        request, "blog/post-detail.html", {"post": posts, "comment_form": comment_form}
-    )
+  posts = Post.objects.filter(published_at__lte=timezone.now())
+  return render(request, "blog/index.html", {"posts": posts})
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    return render(request, "blog/post-detail.html", {"post": post})
